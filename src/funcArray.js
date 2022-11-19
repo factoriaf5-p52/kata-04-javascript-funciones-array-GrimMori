afbe1b1fa9ada1b5f7e717a0de372f0e0e033b42
GrimMori <armigofire@gmail.com>
2022-11-19 02:57:37 +0100

    exercises 1 and 2 completed

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,7 +1,25 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(){}
-export function findLongestWord(){}
+export function maxOfTwoNumbers(a, b){
+    if(a>b){
+        return a;
+    } else if(b>a){
+        return b;
+    } else {
+        return a;
+    }
+}
+
+export function findLongestWord(list){
+    if(list.length < 2){
+        return list[0];
+    } else {
+        list.sort((a, b) => b.length - a.length);
+        console.log(list[0])
+        return list[0];
+    }
+}
+
 export function sumArray(){}
 export function averageNumbers(){}
 export function averageWordLength(){}
